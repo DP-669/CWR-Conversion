068a6f22f41b87471db3113a286bb91b6b111f10
DP-669 <130940520+DP-669@users.noreply.github.com>
2026-01-26 20:18:04 -0800

    Update mapping configuration with new entries

--- mapping_config.py
+++ mapping_config.py
@@ -5,7 +5,6 @@ SUBMITTER_INFO = {
     "id": "01254514077"
 }
 
-# Sub-Publisher details (always Lumina for your ICE feed)
 SUB_PUB = {
     "name": "LUMINA PUBLISHING UK",
     "ipi": "01254514077",
@@ -13,17 +12,26 @@ SUB_PUB = {
     "territory": "0826"
 }
 
-# Internal IDs and Agreement numbers from your approved file
+SOCIETY_MAP = {
+    "BMI": "021", "PRS": "052", "MCPS": "033", "ASCAP": "010", 
+    "SOCAN": "045", "GEMA": "042", "SACEM": "006", "SABAM": "002"
+}
+
 PUBLISHER_MAPPING = {
     "TARMAC 1331 PUBLISHING": {"agreement": "6781310", "ipi": "00356296239", "internal_id": "000000001"},
     "PASHALINA PUBLISHING": {"agreement": "4316161", "ipi": "00498578867", "internal_id": "000000006"},
     "MANNY G MUSIC": {"agreement": "13997451", "ipi": "00515125979", "internal_id": "000000013"},
-    "REDCOLA PUBLISHING": {"agreement": "4165777", "ipi": "00420164014", "internal_id": "000000000"},
+    "TARMAC 1332": {"agreement": "13992037", "ipi": "00420164014", "internal_id": "000000018"},
+    "REDCOLA PUBLISHING": {"agreement": "4165777", "ipi": "00420164014", "internal_id": "000000007"},
     "MINA PUBLISHING": {"agreement": "13995081", "ipi": "01079343442", "internal_id": "000000015"},
-    "LUKACINO PUBLISHING": {"agreement": "3845006", "ipi": "00556764020", "internal_id": "000000000"},
-    "SNOOPLE SONGS": {"agreement": "13990221", "ipi": "00610526488", "internal_id": "000000000"},
-    "TORO ROSSO TRACKS": {"agreement": "13994607", "ipi": "00705678622", "internal_id": "000000000"}
+    "SNOOPLE SONGS": {"agreement": "13990221", "ipi": "01079343442", "internal_id": "000000009"},
+    "LUKACINO PUBLISHING": {"agreement": "3845006", "ipi": "01254514077", "internal_id": "000000000"}
 }
 
-# Required to prevent import errors
-WRITER_MAPPING = {}
+# Approved Writer IDs extracted from your 117k line file
+WRITER_ID_MAP = {
+    "WHEELER": "000000001", "PRICE": "000000002", "WARDA": "000000094",
+    "REINMULLER": "000000107", "PELOSI": "000000108", "STANCZYKIEWICZ": "000000109",
+    "SCHIFF": "000000095", "TAL": "000000096", "BARLETTA": "000000127",
+    "BURT": "000000115", "WATT": "000000011", "MCDONALD": "000000089"
+}
