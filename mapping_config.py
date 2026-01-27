8c3e8f7bc80ce71d1d728fdaf4b4f8c337eb20f2
DP-669 <130940520+DP-669@users.noreply.github.com>
2026-01-26 20:12:09 -0800

    Refactor publisher mapping and submitter details
    Updated publisher mapping and submitter info.

--- mapping_config.py
+++ mapping_config.py
@@ -1,24 +1,29 @@
 # mapping_config.py
 
-# DEDUCED FROM YOUR BIG FILE ("Dust Mites", etc.)
-PUBLISHER_MAPPING = {
-    "Pashalina Publishing": {"agreement": "4316161", "ipi": "00498578867", "internal_id": "02000000006"},
-    "Tarmac 1331 Publishing": {"agreement": "6781310", "ipi": "00356296239", "internal_id": "01000000001"},
-    "Manny G Music": {"agreement": "13997451", "ipi": "00515125979", "internal_id": "03000000013"},
-}
-
-# Ensure this is here even if empty
-WRITER_MAPPING = {} 
-
-SUB_PUB = {
-    "name": "LUMINA PUBLISHING UK",
-    "ipi": "01254514077",
-    "internal_id": "000000012",
-    "role": "SE",
-    "territory": "0826"
-}
-
 SUBMITTER_INFO = {
     "name": "LUMINA PUBLISHING UK",
     "id": "01254514077"
 }
+
+# Sub-Publisher details (always Lumina for your ICE feed)
+SUB_PUB = {
+    "name": "LUMINA PUBLISHING UK",
+    "ipi": "01254514077",
+    "internal_id": "000000012",
+    "territory": "0826"
+}
+
+# Internal IDs and Agreement numbers from your approved file
+PUBLISHER_MAPPING = {
+    "TARMAC 1331 PUBLISHING": {"agreement": "6781310", "ipi": "00356296239", "internal_id": "000000001"},
+    "PASHALINA PUBLISHING": {"agreement": "4316161", "ipi": "00498578867", "internal_id": "000000006"},
+    "MANNY G MUSIC": {"agreement": "13997451", "ipi": "00515125979", "internal_id": "000000013"},
+    "REDCOLA PUBLISHING": {"agreement": "4165777", "ipi": "00420164014", "internal_id": "000000000"},
+    "MINA PUBLISHING": {"agreement": "13995081", "ipi": "01079343442", "internal_id": "000000015"},
+    "LUKACINO PUBLISHING": {"agreement": "3845006", "ipi": "00556764020", "internal_id": "000000000"},
+    "SNOOPLE SONGS": {"agreement": "13990221", "ipi": "00610526488", "internal_id": "000000000"},
+    "TORO ROSSO TRACKS": {"agreement": "13994607", "ipi": "00705678622", "internal_id": "000000000"}
+}
+
+# Required to prevent import errors
+WRITER_MAPPING = {}
